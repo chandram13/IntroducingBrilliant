ae36786df359366e8f2c879a3f99c6db34141b5a
chandram13 <mschandr@uci.edu>
2022-09-12 10:20:25 -0700

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+
+trait Speaker:
+    def firstChallege(): println("The yellow haired Rival wants to battle with you!")
+
+trait FirstSinnohGym:
+    def grunt1(): Unit = println("Geodude, Lvl: 8","Onix, Lvl: 8")
+    def grunt2(): Unit = println("Geodude, Lvl: 10")
+    def gymLeader(): Unit = println("Geodude, Lvl: 12","Onix, Lvl: 12","Cranidos, Lvl: 14")
+trait winterOlympics:
+    def skateboarding: Unit = println("US Represenative: First Name")
+    def swimming: Unit = println("US Representative: Second Name")
+
+class Regions(name: String) extends Kanto,Johto,Hoenn,Sinnoh,Unova,Kalos,Alola,Galar:
+    def completionMessage: String = "Congrats on visiting each of the regions in the Pokemon Universe!"
